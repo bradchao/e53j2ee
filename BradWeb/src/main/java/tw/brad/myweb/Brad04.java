438e233f567ecd4712e1a9819f39de28fafca1e5
BradChao <brad@brad.tw>
2022-12-05 14:00:55 +0800

    12051400

--- BradWeb/src/main/java/tw/brad/myweb/Brad04.java
+++ BradWeb/src/main/java/tw/brad/myweb/Brad04.java
@@ -1,6 +1,9 @@
 package tw.brad.myweb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Brad04")
 public class Brad04 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+//		Enumeration<String> names = request.getParameterNames();
+//		while (names.hasMoreElements()) {
+//			String name = names.nextElement();
+//			String value = request.getParameter(name);
+//			System.out.printf("%s : %s\n", name, value);
+//		}
+		
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		
+		int intX = Integer.parseInt(x);
+		int intY = Integer.parseInt(y);
+		int result = intX + intY;
+		
+		
+		//-----------
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.printf("%d + %d = %d", intX, intY, result);
+		response.flushBuffer();
+		
+		
+	
 	}
 }
---
+++ BradWeb/src/main/java/tw/brad/myweb/Brad05.java
@@ -0,0 +1,31 @@
+package tw.brad.myweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Brad05")
+public class Brad05 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		
+		//------------
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<form action='Brad05'>");
+		out.print("<input name='x'/>");
+		out.print(" + ");
+		out.print("<input name='y'/> ");
+		out.print("<input type='submit' value='='>");
+		out.print("</form>");
+		
+		response.flushBuffer();		
+		
+	}
+}
